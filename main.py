f3495f36d7761a4d18e5465416f26780a01db48c
Shubh07 <shubhanshu.k@gmail.com>
2020-06-06 18:46:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import random
+
+class Card (object):
+  def __init__(self,suit,value):
+    self.suite=suit
+    self.value=value
+
+  def show(self):
+    print ( "{} of {}".format(self.value,self.suite))
+
+class Deck(object):
+  def __init__(self):
+    self.cards=[]
+    self.build()
+
+  def build(self):
+    for s in ["Spades","Clubs","hearts","Diamonds"]:
+        for v in range(1,14):
+          self.cards.append(Card(s,v))
+           #print ("{} of {}".format(v,s))
+
+  def show(self):
+    for c in self.cards:
+      c.show()
+
+  def shuffle(self):
+    for i in range(len(self.cards)-1,0,-1):
+      r = random.randint(0,i)
+      self.cards[i],self.cards[r]=self.cards[r],self.cards[i]
+
+  def draw(self):
+    return self.cards.pop()
+
+
+#class Player (object):
+  #def __init__(self):
+
+#card = Card("Clubs",8)
+#card.show()
+
+dec= Deck()
+#dec.show()
+dec.shuffle()
+dec.show()
